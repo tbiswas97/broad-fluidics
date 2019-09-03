48df52798b692aebbc7c6f27aaf2858c47fa1a01
tbiswas97 <tridib.biswas44@gmail.com>
2019-09-03 16:53:44 -0400

    Working sequential pumping

--- DAQ.m
+++ DAQ.m
@@ -1,42 +1,45 @@
-classdef DAQ 
+classdef DAQ < handle
     properties (Access = public)
         currentScan
         DAQSession
     end
     methods (Access = public)
-        function  connect(self)
-            self.DAQSession = daq.createSession('ni');
-            s = self.DAQSession;
+        function  obj = connect(obj)
+            obj.DAQSession = daq.createSession('ni');
+            s = obj.DAQSession;
             addAnalogOutputChannel(s,'Dev1','ao0','Voltage'); %Pump Speed Control 
-            addDigitalOutputChannel(s,'Dev1','Port0/Line0','OutpuOnly'); %Pump Direction Control
-            addDigitalOutputChannel(s,'Dev1','Port0/Line2','OutputOnly');%Pump Power Control
-            addDigitalOutputChannel(s,'Dev1','Port0/Line8:19','OutputOnly'); %Control valves 
+            addDigitalChannel(s,'Dev1','Port0/Line0','OutputOnly'); %Pump Direction Control
+            addDigitalChannel(s,'Dev1','Port0/Line2','OutputOnly');%Pump Power Control
+            addDigitalChannel(s,'Dev1','Port0/Line8:19','OutputOnly'); %Control valves 
+            obj.currentScan = zeros(1,length(s.Channels));
         end
         function switchValve(self,varargin)
             signal = self.currentScan;
-            for i = 1:nargin
-                signal(4*nargin:(4*nargin)+3) = dec2mat(varargin{i});
+            nargin_wc = nargin - 1;
+            for i = 1:nargin_wc
+                signal(4*i:(4*i)+3) = dec2mat(varargin{i});
             end
             signal (3) = 1;
             outputSingleScan(self.DAQSession, signal)
         end
-        function primeTubing(self)
-            self.currentScan(1) = 5;
-            self.currentScan(3) = 0;
-            outputSingleScan(self.DAQSession,self.currentScan);
+        function obj = primeTubing(obj)
+            obj.currentScan(1) = 5;
+            obj.currentScan(3) = 0;
+            outputSingleScan(obj.DAQSession,obj.currentScan);
         end
         function startFlow(self,num,unit)
             if strcmp(unit,'mL/min')
                 voltage = (num - 0.202)/0.456;
                 vr = round(voltage,2);
                 self.currentScan(1) = vr;
+                self.currentScan(3) = 0;
                 outputSingleScan(self.DAQSession,self.currentScan)
             else
                 disp('Only mL inputs are supported at this time')
             end
         end 
         function stopPump(self)
-            self.currentScan(3) = 0;
+            self.currentScan(3) = 1;
             self.currentScan(1) = 0;
             outputSingleScan(self.DAQSession,self.currentScan)
         end
--- Experiment1.m
+++ Experiment1.m
@@ -1,19 +1,22 @@
 %Experiment 1 
 daq = DAQ;
-%daq.connect();
-%daq.DAQSession.Rate = 1;
-daq.primeTubing;
+daq.connect();
+daq.DAQSession.Rate = 1;
+%daq.primeTubing;
 disp('Press key when priming complete')
 pause();
 
 fr = input('Set the volume to fill each well (mL)'); 
 
-ro = [1 4 2 4 3 4]; %order of reagents
+ro = [5 5 5 5 5 5]; %order of reagents
 
-for i = 1:4
-    daq.switchValve(ro(i),i);
+wo = [1 2 3 4 5 6];
+for i = 1:6
+    daq.switchValve(ro(i),wo(i));
     pause(1)
-    daq.startFlow(fr,'mL')
-    fprintf('Filling well %d with reagent %d ...\n',i, ro(i))
-    pause(fr*60)
+    daq.startFlow(fr,'mL/min')
+    fprintf('Filling well %d with reagent %d ...\n',wo(i), ro(i))
+    pause(60)
 end
+
+daq.stopPump();
